d411e7b9c150aa7364be66a857a32575151d1e37
Eren Gazioglu <eren.g94@gmail.com>
2025-11-13 09:42:22 +0100

    check for + on atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 18:20:54 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/10 16:10:37 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/13 01:37:27 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ int	ft_atoi(const char *str)
 	acc = 0;
 	if (!ft_strncmp(str, "-2147483647", 11))
 		return (-2147483647);
-	if (*str == '-')
+	if (*str == '-' || *str == '+')
 	{
-		sign *= -1;
+		if (*str == '-')
+			sign *= -1;
 		str++;
 	}
 	while (*str)
