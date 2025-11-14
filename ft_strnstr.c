568eac999333c39e84c75161cc340163f1161c76
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-14 14:05:36 +0100

    fix strnstr bugs

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 18:36:35 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/14 11:47:32 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/14 14:04:45 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	needle_len;
+
 	if (!(*needle))
 		return ((char *) haystack);
 	if (!len)
 		return (NULL);
-	while (*haystack && len--)
+	needle_len = ft_strlen(needle);
+	while (*haystack && len-- >= needle_len)
 	{
 		if (*haystack == *needle)
 			if (!ft_strncmp(haystack, needle, ft_strlen(needle)))
