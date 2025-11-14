f2f46529652f6121863c7e595944168044f0d81d
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-14 15:49:02 +0100

    Scaffold additional fn files

--- Makefile
+++ Makefile
@@ -28,7 +28,18 @@ SRCS = \
 	ft_memcmp.c \
 	ft_memchr.c \
 	ft_memmove.c \
-	ft_memset.c
+	ft_memset.c \
+	ft_itoa.c \
+	ft_putchar_fd.c \
+	ft_putendl_fd.c \
+	ft_putnbr_fd.c \
+	ft_putstr_fd.c \
+	ft_split.c \
+	ft_striteri.c \
+	ft_strjoin.c \
+	ft_strmapi.c \
+	ft_strtrim.c \
+	ft_substr.c
 
 OBJS := $(SRCS:%.c=obj/%.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 00:11:29 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/14 15:01:46 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/14 15:37:23 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stddef.h>
 # include <stdlib.h>
 
+// Part 1: Libc functions
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -39,5 +40,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
+
+// Part 2: Additional functions
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
 
 #endif
