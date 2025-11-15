d4b62a0cf0174993a0b30e59add9ab11072a870e
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-15 12:12:06 +0100

    complete substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,28 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:43:14 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/14 15:46:17 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/15 11:36:22 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*temp;
+	char	*ptr;
+
+	temp = ft_calloc(len + 1, sizeof(char));
+	if (!temp)
+		return (NULL);
+	if (len == 0 || start > ft_strlen(s))
+		return (temp);
+	len += start;
+	ptr = temp;
+	while (start < len)
+	{
+		*(temp++) = s[start];
+		start++;
+	}
+	return (ptr);
+}
