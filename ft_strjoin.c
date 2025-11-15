9899213c6ddc8225d5f8939d12d45cff7ea82d12
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-15 12:21:08 +0100

    solve strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,22 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:42:44 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/14 15:42:45 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/15 12:20:39 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strjoin(char const *s1, char const *s2);
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	int		len;
+	
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = ft_calloc(len + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, len + 1);
+	ft_strlcat(result, s2, len + 1);
+	return (result);
+}
