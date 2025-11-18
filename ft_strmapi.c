25518575ed8c628ec4b7443080dc4f36023b0b9e
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-18 01:04:19 +0100

    solve strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,8 +6,25 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:42:56 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/14 15:42:56 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/18 01:03:46 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*copy;
+	int		i;
+
+	copy = ft_strdup(s);
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (copy[i])
+	{
+		copy[i] = f(i, copy[i]);
+		i++;
+	}
+	return (copy);
+}
