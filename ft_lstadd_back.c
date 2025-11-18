35fc280a8c6ecdc51075926d74f3ce60298c0390
POMPY PRODUCTIONS <accounts@pompyproductions.com>
2025-11-18 11:13:26 +0100

    solve lstadd functions

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,10 +6,20 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 03:14:07 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/18 03:26:34 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/18 11:12:55 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void 	ft_lstadd_back(t_list **lst, t_list *new);
+void 	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!(*lst))
+		*lst = new;
+	else
+	{
+		while ((*lst)->next)
+			*lst = (*lst)->next;
+		(*lst)->next = new;
+	}
+}
