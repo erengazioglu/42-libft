d27d530e3207c19c05576d593976c11ce4039771
Eren Gazioglu <egaziogl@f2r3s17.clusters.42paris.fr>
2025-11-18 16:29:38 +0100

    solve list clear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,10 +6,20 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 03:14:53 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/18 03:26:41 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/18 16:29:28 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void 	ft_lstclear(t_list **lst, void (*del)(void *));
+void 	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next;
+	
+	while (*lst)
+	{
+		next = (*lst)->next;
+		del(*lst);
+		*lst = next;
+	}
+}
