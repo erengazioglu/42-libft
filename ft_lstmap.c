f2c9c707e41ba8a84cde2901137a12eea9c01387
Eren Gazioglu <eren.g94@gmail.com>
2025-11-19 05:29:17 +0100

    conform to norm

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: egaziogl <egaziogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 03:15:30 by egaziogl          #+#    #+#             */
-/*   Updated: 2025/11/19 05:27:14 by egaziogl         ###   ########.fr       */
+/*   Updated: 2025/11/19 05:28:58 by egaziogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	new_lst = ft_lstnew(f(lst->content));
 	if (!new_lst)
 		return (NULL);
-	// lst = lst->next;
 	while (lst->next)
 	{
 		lst = lst->next;
@@ -37,48 +36,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (new_lst);
 }
-
-// tests
-
-// static void	del_node(void *lst)
-// {
-// 	if (((t_list *)lst)->content)
-// 		free(((t_list *)lst)->content);
-// 	free(lst);
-// }
-
-// static void	*str_uppercase(void *ptr)
-// {
-// 	char	*str = (char *)ptr;
-
-// 	while (*str)
-// 	{
-// 		*str = ft_toupper(*str);
-// 		str++;
-// 	}
-// 	return (ptr);
-// }
-
-// int	main(void)
-// {
-// 	t_list	*lst = ft_lstnew(ft_strdup("hello"));
-// 	t_list	*node;
-// 			// t_list	*l = lstnew(strdup(" 1 2 3 "));
-// 			// t_list	*ret;
-
-// 			// l->next = lstnew(strdup("ss"));
-// 			// l->next->next = lstnew(strdup("-_-"));
-
-
-// 	lst->next = ft_lstnew(ft_strdup("hiya"));
-// 	lst->next->next = ft_lstnew(ft_strdup("-_-"));
-
-// 	printf("content: %s\n", (char *) lst->content);
-// 	printf("content: %s\n", (char *) lst->next->content);
-// 	ft_lstmap(lst, str_uppercase, del_node);
-// 	printf("modded content: %s\n", (char *) lst->content);
-// 	printf("modded content: %s\n", (char *) lst->next->content);
-// 	printf("modded content: %s\n", (char *) lst->next->next->content);
-
-
-// }
